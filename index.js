c34fe89c64ee6c10927633da9f0e6542942a8b2b
Olov Lassus <olov.lassus@gmail.com>
2016-11-17 21:30:06 +0100

    remove unused package

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 ReactDOM.render(React.createElement("span", null, "hello"), document.getElementById("root"));
+asdfjjafsdiasdjfoiajsdfiasdjfoiajsdfoijaosidjfoiasdjasdfasdfasasdfasdfasdfasdfasdfasdfasdfasdf
