52c14a6c8af9fbd692cc2ec26f47f7ccb1d67907
Olov Lassus <olov.lassus@gmail.com>
2016-11-17 21:32:01 +0100

    repo

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 ReactDOM.render(React.createElement("span", null, "hello"), document.getElementById("root"));
-asdfjjafsdiasdjfoiajsdfiasdjfoiajsdfoijaosidjfoiasdjasdfasdfasasdfasdfasdfasdfasdfasdfasdfasdf
