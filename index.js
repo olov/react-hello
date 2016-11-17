10d9baf9ddaa2f81b4a93fc57460b8457b301a0a
Olov Lassus <olov.lassus@gmail.com>
2016-11-17 21:14:56 +0100

    can it be simpler?

---
+++ index.js
@@ -0,0 +1,3 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("span", null, "hello"), document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    entry: "./index.js",
+    output: { filename: "./dist/bundle.js" },
+    devtool: "source-map",
+};
