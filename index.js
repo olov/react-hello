27dfd33eb410e5f4d77a25ad03ec129c452fc442
Olov Lassus <olov.lassus@gmail.com>
2016-11-17 21:15:44 +0100

    look ma no es6

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+var React = require("react");
+var ReactDOM = require("react-dom");
 ReactDOM.render(React.createElement("span", null, "hello"), document.getElementById("root"));
