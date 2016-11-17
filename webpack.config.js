d5bf96a96d0b115259c779baad17a581777ff417
Olov Lassus <olov.lassus@gmail.com>
2016-11-17 21:16:41 +0100

    externalize react

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,8 @@ module.exports = {
     entry: "./index.js",
     output: { filename: "./dist/bundle.js" },
     devtool: "source-map",
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
